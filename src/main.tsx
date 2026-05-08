76532a562785f6fb35f8a13cc76ba14d88035e30
lwooden <lowell.wooden@gmail.com>
2026-05-07 21:17:13 -0400

    banner update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<head>
+			<title>S3 Uploader</title>
+		</head>
+		<div className="h-7 w-full bg-yellow-300">
+			<p className="text-black font-bold text-center">
+				TS/SCI Site Security Clearance
+			</p>
+		</div>
+		<App />
+	</StrictMode>,
+);
